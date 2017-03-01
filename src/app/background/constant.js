68db6eafff4eda107fcf66c96018c0b6061925db
lelabo_m <marc.lelabourier@gmail.com>
2017-03-01 15:24:15 +0100

    background: new constant file.

--- src/app/background/constant.js
+++ src/app/background/constant.js
@@ -1,15 +1,8 @@
 /**
- * Created by lelabo on 27/02/17.
+ * Created by lelabo on 01/03/17.
  */
-'use strict';
-angular.module('advbookmarks-bg', []).constant("global", {
+angular.module('advbookmarks-bg').constant("global", {
     "root_dir": "adv-bookmarks-inbox",
     "read_dir": "to_read",
     "interest_dir": "interesting"
-}).run(function(global) {
-    console.log(global.root_dir);
 });
-
-// angular.module('advbookmarks-bg', []).run(function() {
-//     console.log("HELLO WORLD!");
-// });
