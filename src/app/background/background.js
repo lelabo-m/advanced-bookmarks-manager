89285963a3191e7fcb8007fa8bc8b45a341302f1
lelabo_m <marc.lelabourier@gmail.com>
2017-03-01 15:24:44 +0100

    background: new angular entry point

--- src/app/background/background.js
+++ src/app/background/background.js
@@ -9,5 +9,17 @@
 // init_extension();
 //
 
-// var app = angular.module('advbookmarks-bg');
+var app = angular.module('advbookmarks-bg', []);
+
+// app.constant("global", {
+//         "root_dir": "adv-bookmarks-inbox",
+//         "read_dir": "to_read",
+//         "interest_dir": "interesting"
+// });
+
+app.run(function(SystemService) {
+        console.log("RUN");
+        // console.log(global.root_dir);
+        SystemService.init();
+});
 
