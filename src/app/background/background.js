fe1b79afbb05cef8e9dbaeed08dd493fd6350400
lelabo_m <marc.lelabourier@gmail.com>
2017-03-09 12:00:15 +0100

    Update chrome extension entry point

--- src/app/background/background.js
+++ src/app/background/background.js
@@ -1,25 +1,6 @@
-// var global = new Object();
-//
-// function init_extension() {
-//     var root_dir = BookmarkQuery.Directory(constant.root_dir);
-//     var root = BookmarksManager.create(root_dir);
-//     Debug.inspect_object(root)
-// }
-//
-// init_extension();
-//
-
 var app = angular.module('advbookmarks-bg', []);
 
-// app.constant("global", {
-//         "root_dir": "adv-bookmarks-inbox",
-//         "read_dir": "to_read",
-//         "interest_dir": "interesting"
-// });
-
 app.run(function(SystemService) {
-        console.log("RUN");
-        // console.log(global.root_dir);
         SystemService.init();
 });
 
